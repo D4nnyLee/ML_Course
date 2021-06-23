a719a4c011bfc0fe8a7c2bd026ab3c6fee028e01
D4nnyLee <zxcvbnm04987@gmail.com>
2021-06-23 16:39:52 +0800

    Use decision tree algorithm to play

--- ml_play.py
+++ ml_play.py
@@ -3,12 +3,12 @@ The template of the script for the machine learning process in game pingpong
 '''
 
 # Import the necessary modules and classes
-import pickle
 import os
 import sys
+import pickle
 
-model_name_1p = 'MLP_1P.pickle'
-model_name_2p = 'MLP_2P.pickle'
+model_name_1p = 'DT_1P.pickle'
+model_name_2p = 'DT_2P.pickle'
 
 with open(os.path.join(os.path.dirname(__file__), model_name_1p), 'rb') as f:
     model_1p = pickle.load(f)
@@ -24,8 +24,8 @@ class MLPlay:
         @param side A string "1P" or "2P" indicates that the `MLPlay` is used by
                which side.
         """
-        self.ball_served = False
         self.side = side
+        self.ball_served = False
         self.last_blocker_pos_x = None
 
         if self.side == '1P':
@@ -45,17 +45,14 @@ class MLPlay:
             self.last_blocker_pos_x = scene_info['blocker'][0]
             return 'SERVE_TO_LEFT'
 
-        command = self.__move_to(scene_info[f'platform_{self.side}'][0], self.__predict_x(scene_info))
-
-        self.last_blocker_pos_x = scene_info['blocker'][0]
-
-        return command
+        return self.__move_to(scene_info[f'platform_{self.side}'][0] + 20, self.__predict_x(scene_info))
 
     def reset(self):
         """
         Reset the status
         """
         self.ball_served = False
+        self.last_blocker_pos_x = None
 
     def __move_to(self, curr: int, pred: int) -> {0, 1, 2}:
         if pred - 3 < curr and curr < pred + 3:
@@ -66,6 +63,11 @@ class MLPlay:
             return 'MOVE_LEFT'
 
     def __predict_x(self, scene_info) -> int:
-        return self.model.predict([
-            [*scene_info['ball'], *scene_info['ball_speed'], *scene_info['blocker'], self.last_blocker_pos_x - scene_info['blocker'][0]]
+        pred = self.model.predict([
+            [*scene_info['ball'], *scene_info['ball_speed'],
+                *scene_info['blocker'], scene_info['blocker'][0] - self.last_blocker_pos_x]
             ])[0]
+
+        self.last_blocker_pos_x = scene_info['blocker'][0]
+
+        return pred
