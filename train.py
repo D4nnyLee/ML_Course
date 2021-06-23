b843169b94fa47807a7808510ef1f0ecf0e8d33f
D4nnyLee <zxcvbnm04987@gmail.com>
2021-06-23 16:47:17 +0800

    Rewrite train.py
    * Make it more readable
    * Improve the performance
        To avoid overfitting, I don't want to add duplicate features
        into test data.
    
        At first, I use `in` operator to check if there is already a
        identical feature in test data.
    
        But the time complexity of this operation will be O(N) since
        list is unhashable in default.
    
        Thus I develop my own hash function and use `set` to achieve
        time complexity of O(N * log(N))

--- train.py
+++ train.py
@@ -1,9 +1,11 @@
-#!/usr/bin/env python3
+#!python3
 
 import os
 import pickle
 
-from sklearn.neighbors import KNeighborsClassifier
+import numpy as np
+
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
@@ -11,66 +13,106 @@ def log_obj(obj, name):
     with open(name, 'wb') as f:
         pickle.dump(obj, f)
 
-def collide_with(side: str, scene_info) -> bool:
-    ball_size = (5, 5)
-    platform_size = (40, 30)
+def hash_list(l: list):
+    assert len(l) == 7
 
-    if side == '1P' and scene_info['ball'][1] + ball_size[1] == scene_info['platform_1P'][1]:
+    bit_len = 10
+    ret = 0
+    for i in range(7):
+        val = l[i] + 2 ** (bit_len - 1)
+
+        assert 0 <= val and val < 2 ** bit_len
+        ret <<= bit_len
+        ret |= val
+    return ret
+
+def collide(side, scene_info) -> bool:
+    if side == '1P' and scene_info['ball'][1] >= 415:
         return True
-    if side == '2P' and scene_info['platform_2P'][1] + platform_size[1] == scene_info['ball'][1]:
+    if side == '2P' and scene_info['ball'][1] <= 80:
         return True
     return False
 
 def train(side: str):
-    data_dir = '../MLGame/games/pingpong/log'
+    data_dir = os.path.join(os.path.dirname(__file__), '..', 'log')
 
-    x = []
-    y = []
+    fd = None
 
+    '''
+    Prepare dataset
+    '''
+    print('start preparing dataset...')
+    x, y = [], []
+    in_x = set()
     for filename in os.listdir(data_dir):
-        f = open(os.path.join(data_dir, filename), 'rb')
-        all_scene_info = pickle.load(f)
-        f.close()
+        with open(os.path.join(data_dir, filename), 'rb') as f:
+            data = pickle.load(f)
+            assert data['ml_1P']['scene_info'] == data['ml_2P']['scene_info']
+            scene_info = data[f'ml_{side}']['scene_info']
 
-        last_blocker_x = 85
-        last_frame = 0
-        tmp = [[], []]
+        length = len(scene_info)
 
-        for scene_info in all_scene_info:
-            tmp[0].append([*scene_info['ball'], *scene_info['ball_speed'], *scene_info['blocker'], last_blocker_x - scene_info['blocker'][0]])
-            last_blocker_x = scene_info['blocker'][0]
+        '''
+        Convert scene_info to features
+        '''
+        last_blocker_x = scene_info[0]['blocker'][0]
 
-            if collide_with(side, scene_info):
-                for _ in range(last_frame, scene_info['frame']):
-                    tmp[1].append(scene_info['ball'][0])
-                last_frame = scene_info['frame']
+        features = []
+        for info in scene_info:
+            curr_blocker_x = info['blocker'][0]
 
-        x.extend(tmp[0][:len(tmp[1])])
-        y.extend(tmp[1])
+            features.append([*info['ball'], *info['ball_speed'],
+                *info['blocker'], curr_blocker_x - last_blocker_x])
 
-    '''
-    orig_len = len(x)
-    for i in range(orig_len):
-        if y[i] - 1 >= 0:
-            x.append(x[i])
-            y.append(y[i] - 1)
-        if y[i] + 1 <= 200:
-            x.append(x[i])
-            y.append(y[i] + 1)
-    '''
+            last_blocker_x = curr_blocker_x
+
+        assert len(features) == length
+
+        '''
+        Create label
+        '''
+        pending = []
+        for i in range(length):
+            if collide(side, scene_info[i]):
+                for p in pending:
+                    hash_val = hash_list(p)
+
+                    if hash_val in in_x:
+                        continue
+
+                    x.append(p)
+                    y.append(scene_info[i]['ball'][0] + 2) # x coordinate of the middle of the ball
+                    in_x.add(hash_val)
+
+                pending.clear()
+            else:
+                pending.append(features[i])
+
+    assert len(x) == len(y)
+    assert len(x) == len(in_x)
+    print('done')
+
+    x = np.array(x)
+    y = np.array(y)
+
+    print(x.shape)
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = .2)
 
-    knn = KNeighborsClassifier(2)
+    '''
+    Create model
+    '''
+    clf = DecisionTreeClassifier(random_state = 0)
+    print('start training...')
+    clf.fit(x_train, y_train)
+    print('done')
 
-    knn.fit(x_train, y_train)
+    y_pred = clf.predict(x_test)
 
-    y_pred = knn.predict(x_test)
+    print(f'accuracy_score: {accuracy_score(y_test, y_pred) * 100}%')
 
-    print('Accuracy score: %2d%%' % (accuracy_score(y_test, y_pred) * 100))
-
-    log_obj(knn, 'KNN_%s.pickle' % side)
+    log_obj(clf, f'DT_{side}.pickle')
 
 if __name__ == '__main__':
-    # train('1P')
+    train('1P')
     train('2P')
